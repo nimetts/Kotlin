49e17f2d708807247ae59af548ad80d36b807497
nimetts <105562039+nimetts@users.noreply.github.com>
2024-02-07 18:57:59 +0300

    superhero creatin function generated

--- app/src/main/java/com/nimetseyrek/myfirstandroidproject/MainActivity.kt
+++ app/src/main/java/com/nimetseyrek/myfirstandroidproject/MainActivity.kt
@@ -3,6 +3,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import com.nimetseyrek.myfirstandroidproject.databinding.ActivityMainBinding
 
@@ -30,9 +31,31 @@ class MainActivity : ComponentActivity() {
         // Access views using View Binding
         binding.textView.text = "Merve Akdeniz"
     }*/
+    }
+    fun superKahramanYap(view : View){
+         // Layout dosyanızdaki plain text alanının ID'si "isimText" olsun
+        val isimText: EditText = findViewById(R.id.isimText)
+        val yasText: EditText = findViewById(R.id.yasText)
+        val meslekText: EditText = findViewById(R.id.meslekText)
+        // Plain text alanının içeriğine erişmek için
+        val isim = isimText.text.toString()
+        val yas = yasText.text.toString().toIntOrNull()
+        val meslek = meslekText.text.toString()
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if(yas==null){
+            binding.textView.text = "Doğru Yaşı Giriniz!"
+        }
+        else{
+            val superKahraman= SuperKahraman(isim,yas,meslek)
+            binding.textView.text = "İsim: ${superKahraman.isim} Yaş: ${superKahraman.yas} Meslek: ${superKahraman.meslek} "
+        }
+    }
 
 
 
-}}
+
+
 
 
