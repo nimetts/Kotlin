7bf4a9cd3b7bf4bc2719c3438d0a414455d4b4d0
nimetts <105562039+nimetts@users.noreply.github.com>
2024-02-09 15:25:23 +0300

    second activity created

--- app/src/main/java/com/nimetseyrek/myfirstandroidproject/MainActivity.kt
+++ app/src/main/java/com/nimetseyrek/myfirstandroidproject/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.nimetseyrek.myfirstandroidproject
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -7,50 +8,20 @@ import android.widget.EditText
 import androidx.activity.ComponentActivity
 import com.nimetseyrek.myfirstandroidproject.databinding.ActivityMainBinding
 
+
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("Merhaba Kotlin")
-        var a=5
-        var b= 20
-        println(a*b)
-        /*val button: Button = findViewById(R.id.button)
-        button.setOnClickListener {
-            var binding = ActivityMainBinding.inflate(layoutInflater)
-            setContentView(binding.root)
-            // Access views using View Binding
-            binding.textView.text = "Merve Akdeniz"
-        }*/
-
-   /* fun degistir(view : View) {
-        var binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        // Access views using View Binding
-        binding.textView.text = "Merve Akdeniz"
-    }*/
     }
-    fun superKahramanYap(view : View){
-         // Layout dosyanızdaki plain text alanının ID'si "isimText" olsun
-        val isimText: EditText = findViewById(R.id.isimText)
-        val yasText: EditText = findViewById(R.id.yasText)
-        val meslekText: EditText = findViewById(R.id.meslekText)
-        // Plain text alanının içeriğine erişmek için
-        val isim = isimText.text.toString()
-        val yas = yasText.text.toString().toIntOrNull()
-        val meslek = meslekText.text.toString()
-        var binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        if(yas==null){
-            binding.textView.text = "Doğru Yaşı Giriniz!"
-        }
-        else{
-            val superKahraman= SuperKahraman(isim,yas,meslek)
-            binding.textView.text = "İsim: ${superKahraman.isim} Yaş: ${superKahraman.yas} Meslek: ${superKahraman.meslek} "
-        }
+    fun aktiviteDegistir(view: View){
+    val intent= Intent(applicationContext,SecondActivity::class.java)
+        val kullaniciVerisi: EditText = findViewById(R.id.editText)
+        val veri = kullaniciVerisi.text.toString()
+        intent.putExtra("yollananVeri", veri)
+        startActivity(intent)
+}
     }
 
 
